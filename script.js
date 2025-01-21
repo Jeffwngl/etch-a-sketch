2c361c1e2a1e341000419f6ce9b40ca70ab853df
Jeffwngl <143241899+Jeffwngl@users.noreply.github.com>
2025-01-22 10:41:19 +1100

    Changed class names and basic js

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("container");
+
+function populateContainer() {
+    document.createElement("div");
+    
+}
+
+// for (let i = 0; i < 16; i++) {
+//     document.body.appendChild(container);
+// }
