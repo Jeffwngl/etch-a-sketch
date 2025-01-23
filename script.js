29bff2eb7f6c0a6dab0e88780f03b25f615f16f1
Jeffwngl <143241899+Jeffwngl@users.noreply.github.com>
2025-01-23 19:28:17 +1100

    Added functionality to the canvas. Added functionality to submit button to draw grids of up to 500x500.

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-const container = document.querySelector("container");
+// check DOM loaded
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+  });
 
-function populateContainer() {
-    document.createElement("div");
-    
+const canvas = document.querySelector(".drawingBoard");
+const drawCanvas = document.getElementById("submit");
+const width = document.getElementById("width");
+const height = document.getElementById("height");
+const notify = document.querySelector(".notify");
+let color = 'black';
+
+// populate canvas
+function populateContainer(canvas, width, height, notify) {
+    // dimension check
+    if (width != height) {
+        notify.textContent = `Please choose a square canvas size! ^^`;
+        return
+    }
+    // size check
+    if (width > 500 || height > 500) {
+        notify.textContent = `Please choose a smaller size! ^^`;
+        return
+    }
+
+    // removes all squares from previous grid if it exists
+    let squares = canvas.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+
+    let area = height * width;
+
+    // creates grid
+    canvas.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${height}, 1fr)`;
+
+    // populates grid
+    for (i = 0; i < area; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+        square.style.backgroundColor = 'white';
+        canvas.insertAdjacentElement('beforeend', square);
+    }
 }
 
-// for (let i = 0; i < 16; i++) {
-//     document.body.appendChild(container);
-// }
+drawCanvas.addEventListener('click', () => populateContainer(canvas, width.value, height.value, notify));
