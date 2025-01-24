1f426501436c2d68df05008e5c1353be02365880
Jeffwngl <143241899+Jeffwngl@users.noreply.github.com>
2025-01-24 18:47:29 +1100

    Added color wheel and erased individual colors.

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ const drawCanvas = document.getElementById("submit");
 const width = document.getElementById("width");
 const height = document.getElementById("height");
 const notify = document.querySelector(".notify");
+const colorPicker = document.getElementById("colorDisplay");
 let color = 'black';
+let area = 0;
 
 // populate canvas
-function populateContainer(canvas, width, height, notify) {
+function populateContainer(canvas, width, height, notify, area) {
     // dimension check
     if (width != height) {
         notify.textContent = `Please choose a square canvas size! ^^`;
@@ -27,7 +29,7 @@ function populateContainer(canvas, width, height, notify) {
     let squares = canvas.querySelectorAll("div");
     squares.forEach((div) => div.remove());
 
-    let area = height * width;
+    area = height * width;
 
     // creates grid
     canvas.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
@@ -44,4 +46,18 @@ function populateContainer(canvas, width, height, notify) {
     }
 }
 
-drawCanvas.addEventListener('click', () => populateContainer(canvas, width.value, height.value, notify));
+colorPicker.addEventListener('input', (event) => {
+
+});
+
+// function changeColor(color, area) {
+//     for (i = 0; i < area; i++) {
+//         canvas. // start here
+//     }
+// }
+
+// function clear(area) {
+
+// }
+
+drawCanvas.addEventListener('click', () => populateContainer(canvas, width.value, height.value, notify, area));
